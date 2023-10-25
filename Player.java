f0e4102d520b3701fa270d1f91e7efc978401afe
christophkp <kochpaizchristoph@gmail.com>
2023-10-25 12:41:11 -0400

    Added more methods to Player to implement

--- Player.java
+++ Player.java
@@ -2,31 +2,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private int money;
+    private int balance;
     private List<Card> hand;
 
+    private boolean isFolded;
+
     public Player() {
-        this.money = 500;
+        this.balance = 500;
         this.hand = new ArrayList<Card>();
+        this.isFolded = false;
     }
 
     public void playerHand(Card dealtCard) {
         hand.add(dealtCard);
     }
+
     //fix getter to return a copy not a reference
     public List<Card> getPlayerHand()
     {
         return hand;
     }
 
-    public int getPlayerBalance() {
-        return money;
+    public void addBalance(int money)
+    {
+        balance+=money;
     }
 
-    public void setPlayerBalance(int money)
-    {
-        this.money = money;
+    public int getPlayerBalance() {
+        return balance;
     }
+
+    public void fold()
+    {
+        isFolded = true;
+    }
+
+    public boolean getFoldStatus()
+    {
+        return isFolded;
+    }
+
+    public void check()
+    {
+
+    }
+
+    public void call()
+    {
+
+    }
+
+    public void raise(int raiseAmount)
+    {
+
+    }
+
 //    public static void main(String[] args)
 //    {
 //        Deck deck = new Deck();
