fb8cd758efa247cda262b1f591cb7f6d75e3c280
Mason Hall <mlhall1@loyola.edu>
2023-10-28 14:15:26 -0400

    incomplete filler code in getPlayerChoice()

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Player {
 
@@ -69,6 +70,36 @@ public class Player {
      */
     public int getPlayerChoice(int open_bet ) {
         // get a player's choice on their turn
+
+        /*
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        System.out.println( name + ", please enter the number corresponding to your choice:\n");
+        if( totalRoundBet < open_bet ) {
+            System.out.println("CALL\t0\nRAISE\tN\nFOLD\t-1\n");
+            choice = scan.nextInt();
+            switch( choice ) {
+                case 0:
+                    int difference = open_bet - totalRoundBet;
+                    subBalance( difference );
+                    totalRoundBet += difference;
+                    return difference;
+                case -1:
+                    isFolded = true;
+                    return -1;
+                default:
+                    if( choice + totalRoundBet <= open_bet ) { // if they try to ""raise"" below the openbet
+                        return getPlayerChoice(open_bet);
+                    }
+                    subBalance( choice );
+                    totalRoundBet += choice;
+                    return ;
+            }
+        }
+        else {
+            System.out.println("CHECK\t0\nRAISE\tN\nFOLD\t-1\n");
+        }
+        */
         return -1;
     }
     public static void main(String[] args)
