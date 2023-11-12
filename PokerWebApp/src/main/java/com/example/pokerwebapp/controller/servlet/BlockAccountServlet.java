244f5858e8d2146d62b1324b39d5713d9f88e83c
christophkp <kochpaizchristoph@gmail.com>
2023-11-12 16:48:10 -0500

    block functionality and remove friend functionality, buggy needs fixing #11 #13

--- PokerWebApp/src/main/java/com/example/pokerwebapp/controller/servlet/BlockAccountServlet.java
+++ PokerWebApp/src/main/java/com/example/pokerwebapp/controller/servlet/BlockAccountServlet.java
@@ -1,4 +1,33 @@
 package com.example.pokerwebapp.controller.servlet;
 
-public class BlockAccountServlet {
+
+        import com.example.pokerwebapp.controller.service.AccountService;
+        import com.example.pokerwebapp.model.dao.AccountDAO;
+        import com.example.pokerwebapp.model.entity.Account;
+        import jakarta.servlet.annotation.WebServlet;
+        import jakarta.servlet.http.HttpServlet;
+        import jakarta.servlet.http.HttpServletRequest;
+        import jakarta.servlet.http.HttpServletResponse;
+
+        import java.io.IOException;
+
+@WebServlet(name = "blockAccountServlet", value = "/blockAccountServlet")
+public class BlockAccountServlet extends HttpServlet {
+    protected AccountDAO dao = new AccountDAO();
+
+    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Account logged = (Account) request.getSession().getAttribute("Account");
+        String accountUsernameToBlock = request.getParameter("block");
+        Account toBlock = dao.findUserByUsername(accountUsernameToBlock);
+        AccountService.blockAccount(logged,toBlock);
+        response.sendRedirect("testfriendaction.jsp");
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request,response);
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request,response);
+    }
+
 }
